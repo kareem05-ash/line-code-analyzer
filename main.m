558b28abcabee1a3508ced5ae6c87f1a4088fae8
kareem05-ash <kareem.ash05@gmail.com>
2026-04-25 01:15:05 +0300

    edit gen_data.m: make each row represents a single waveform

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ addpath(genpath('src'));
 % ====================
 NUM_WAVEFORMS   = 500;      % no. of waveforms (realizations)
 NUM_BITS        = 100;      % no. of bits per one waveform
-all             = 4;        % Amplitude
+A               = 4;        % Amplitude
 
 % Data Matrix Genratoin
 data = gen_data(NUM_BITS, NUM_WAVEFORMS);
--- src/line_codes/gen_data.m
+++ src/line_codes/gen_data.m
@@ -1,4 +1,4 @@
-% gen_data function returns a num_bits x num_waveforms MATRIX
-function data = gen_data(num_bits, num_waveforms)
-    data = randi([0, 1], num_bits, num_waveforms);
+% gen_data function returns a num_waveforms x num_bits MATRIX
+function data = gen_data(num_waveforms, num_bits)
+    data = randi([0, 1], num_waveforms, num_bits);
 end
